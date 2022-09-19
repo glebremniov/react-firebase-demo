fadc197ddff91e8ad23fad3c7ec1ba7367a3b1f8
glebremniov <g.remniov@gmail.com>
2022-09-19 13:31:51 +0300

    Initial commit

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,45 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import "./Dashboard.css";
+import { auth, db, logout } from "../../config/firebase";
+import { query, collection, getDocs, where } from "firebase/firestore";
+
+function Dashboard() {
+    const [user, loading, error] = useAuthState(auth);
+    const [name, setName] = useState("");
+    const navigate = useNavigate();
+
+    const fetchUserName = useCallback(async () => {
+        try {
+            const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+            const doc = await getDocs(q);
+            const data = doc.docs[0].data();
+            setName(data.name);
+        } catch (err) {
+            console.error(err);
+            alert("An error occured while fetching user data");
+        }
+    }, [user?.uid]);
+
+    useEffect(() => {
+        if (loading) return;
+        if (!user) return navigate("/");
+        fetchUserName();
+    }, [user, loading, navigate, fetchUserName]);
+
+    return (
+        <div className="dashboard">
+            <div className="dashboard__container">
+                Logged in as
+                <div>{name}</div>
+                <div>{user?.email}</div>
+                <button className="dashboard__btn" onClick={logout}>
+                    Logout
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
